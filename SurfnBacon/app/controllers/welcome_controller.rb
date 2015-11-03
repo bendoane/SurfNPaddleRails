8afcb8a4ed8df50fbcbd14af4dde5f372dd982e7
bendoane <ben.doane@gmail.com>
2015-11-03 16:59:55 -0500

    I've got the posts fitting and properly posting via post date

--- SurfnBacon/app/controllers/welcome_controller.rb
+++ SurfnBacon/app/controllers/welcome_controller.rb
@@ -2,11 +2,14 @@ class WelcomeController < ApplicationController
 
 
   def index
-    @post = Post.find(1)
-    @post2 = Post.all
+    @post = Post.all.max_by{|p| p.created_at}
+    @post2 = Post.all.reject{|p| p == @post}
   end
 
   def about
+    @post = Post.find(params[:id].to_i)
+   @posts = Post.all.reject{|p| p == @post}
+   render :template => "post/index.html.erb"
   end
 
 
